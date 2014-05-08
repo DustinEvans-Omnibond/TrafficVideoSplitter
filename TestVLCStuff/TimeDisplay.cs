8fd4f39a8cbd7e68f582cac4903b9c6a12af1b22
Dustin Evans <dustin@omnibond.com>
2014-05-08 16:30:28 -0400

    TimeDisplay Class
    Adding a new class that will make handling time stuff easier.

---
+++ TestVLCStuff/TimeDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficVideoSplitter
+{
+    class TimeDisplay
+    {
+        private TimeSpan time;
+
+        public TimeDisplay()
+        {
+            time = new TimeSpan(0, 0, 0);
+        }
+
+        public TimeDisplay(double ms)
+        {
+            time = new TimeSpan(TimeSpan.FromMilliseconds(ms).Hours, TimeSpan.FromMilliseconds(ms).Minutes, TimeSpan.FromMilliseconds(ms).Seconds);
+        }
+
+        public TimeDisplay(TimeSpan ts)
+        {
+            time = ts;
+        }
+
+        public override string ToString()
+        {
+            return time.ToString();
+        }
+        public TimeSpan GetTimeSpan()
+        {
+            return time;
+        }
+
+        public static TimeDisplay operator +(TimeDisplay t1, TimeDisplay t2)
+        {
+            TimeSpan combined = t1.GetTimeSpan().Add(t2.GetTimeSpan());
+
+            return new TimeDisplay(combined);
+        }
+
+        public static TimeDisplay operator -(TimeDisplay t1, TimeDisplay t2)
+        {
+            TimeSpan combined = t1.GetTimeSpan().Subtract(t2.GetTimeSpan());
+
+            return new TimeDisplay(combined);
+        }
+    }
+}
